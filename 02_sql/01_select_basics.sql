20658fbd20a1c6109d0509d17796bc5ec0912100
Pavko66 <pawelzawadzki52@gmail.com>
2026-07-08 00:43:28 +0200

    Add SQL SELECT basics

--- 02_sql/01_select_basics.sql
+++ 02_sql/01_select_basics.sql
@@ -1,10 +1,23 @@
 -- Projekt 30 – SQL
 -- Temat: SELECT i FROM
 
--- Przykład:
+-- SELECT wybiera kolumny
+-- FROM wskazuje tabelę
+
+-- Przykład 1: wszystkie kolumny z tabeli products
 SELECT *
 FROM products;
 
--- Cel:
--- Zrozumieć, że SELECT wybiera kolumny,
--- a FROM wskazuje tabelę, z której pobieramy dane.
+-- Przykład 2: wybrane kolumny
+SELECT product_name, price
+FROM products;
+
+-- Przykład 3: wybrane kolumny z aliasami
+SELECT 
+    product_name AS nazwa_produktu,
+    price AS cena
+FROM products;
+
+-- Notatka:
+-- SELECT * oznacza: pokaż wszystkie kolumny.
+-- W prawdziwej pracy lepiej wybierać konkretne kolumny, bo jest czytelniej.
