7131660ada006d8cddf885a916b53c71af3dbd52
Pavko66 <pawelzawadzki52@gmail.com>
2026-07-07 23:45:13 +0200

    Add first SQL basics file

---
+++ 02_sql/01_select_basics.sql
@@ -0,0 +1,10 @@
+-- Projekt 30 – SQL
+-- Temat: SELECT i FROM
+
+-- Przykład:
+SELECT *
+FROM products;
+
+-- Cel:
+-- Zrozumieć, że SELECT wybiera kolumny,
+-- a FROM wskazuje tabelę, z której pobieramy dane.
